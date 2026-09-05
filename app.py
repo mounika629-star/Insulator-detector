74cfdbe42c6974cd213b7e1f6658df3b7b1f3ec3
mounika629-star <mounikabandi6291@gmail.com>
2026-09-05 12:27:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-   st.image(image, caption='Uploaded Image', use_container_width=True) 
+    st.image(image, caption='Uploaded Image', use_container_width=True)
     st.write("Detecting defects...")
-    
     results = model.predict(image)
     result_img = results[0].plot()
-    st.image(result_img, caption='Detection Result', use_column_width=True)
+    st.image(result_img, caption='Detection Result', use_container_width=True)
